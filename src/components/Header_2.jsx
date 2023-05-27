e6498d8cabe2d77668b54bff9867fb5f9cde57d1
Rahul Gupta <rahul123alok@gmail.com>
2023-05-27 11:52:18 +0530

    updated header

--- src/components/Header_2.jsx
+++ src/components/Header_2.jsx
@@ -1,11 +1,11 @@
-import Vector from "../assets/img/Vector.png";
+// import Vector from "../assets/img/Vector.png";
 import SearchBar from "./SearchBar";
 
 const Header_2 = ({ title }) => {
   return (
     <div className="header flex h-32 items-center justify-between rounded-b-2xl bg-[#2051E5] p-2 pb-0">
       <div>
-        <img className="absolute left-0 top-0" src={Vector} alt="Logo" />
+        {/* <img className="absolute left-0 top-0" src={Vector} alt="Logo" /> */}
       </div>
 
       <div className="flex items-center justify-center text-white">
