a535065e1e06bae2a6f27a3f392b80cb9591cc34
Rahul Gupta <rahul123alok@gmail.com>
2023-06-11 17:44:25 +0530

    getting task data

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "https://www.google.com",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
-  "http://localhost:5173",
+  "http://localhost:5173"
   // "https://namazzyy.web.app",
 ];
 
