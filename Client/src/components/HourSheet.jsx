cb4e670d5ebc5b9b1845f503407e3ea1cbeb584e
Rahul Gupta <rahul123alok@gmail.com>
2023-06-12 14:04:35 +0530

    Update HourSheet.jsx

--- Client/src/components/HourSheet.jsx
+++ Client/src/components/HourSheet.jsx
@@ -1,58 +1,241 @@
 import { useState, useEffect } from "react";
-import axios from "../api/axios";
 import useContent from "../hooks/useContent";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import jwtDecode from "jwt-decode";
 
-const HourSheet = () => {
+import {
+  CircularProgressbar,
+  CircularProgressbarWithChildren,
+  buildStyles
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+/* ICONS */
+import { CheckboxIndeterminate16Regular } from "@ricons/fluent";
+import { Icon } from "@ricons/utils";
+import { CalendarMonthOutlined } from "@ricons/material";
+
+const HourSheet = ({ selectedDate }) => {
+  const [userInfo, setUserInfo] = useState([]);
+  const [taskData, setTaskData] = useState([]);
+
   const { auth } = useContent();
   const axiosPrivate = useAxiosPrivate();
 
   const TASK_URL = "/tasks";
-  const [userInfo, setUserInfo] = useState([]);
 
   useEffect(() => {
-    const decoded = auth?.accessToken ? jwtDecode(auth.accessToken) : undefined;
-    const userId = decoded?.UserInfo?.id || "";
-    console.log(userId);
-    const handleClick = async () => {
+    const controller = new AbortController();
+
+    const fetchTaskData = async (date) => {
       try {
-        const response = await axiosPrivate.get(`${TASK_URL}`);
-        setUserInfo(response.data);
-        console.log(auth?.accessToken);
-        console.log(response.data);
+        console.log("Fetching data for date:", date);
+
+        const response = await axiosPrivate.get(TASK_URL);
+        const filteredData = response.data.filter((task) => {
+          // Compare the task's date with the selected date (ignoring the time)
+          const taskDate = new Date(task.date).setHours(0, 0, 0, 0);
+          const selectedDateWithoutTime = new Date(date).setHours(0, 0, 0, 0);
+          return taskDate === selectedDateWithoutTime;
+        });
+
+        // Sort the filtered data by taskType (Production first, then Non-Production)
+        filteredData.sort((a, b) => {
+          if (a.taskType === "Production" && b.taskType === "Non-Production") {
+            return -1; // "Production" should come before "Non-Production"
+          } else if (
+            a.taskType === "Non-Production" &&
+            b.taskType === "Production"
+          ) {
+            return 1; // "Non-Production" should come after "Production"
+          } else {
+            return 0; // Preserve the original order
+          }
+        });
+
+        setUserInfo(filteredData);
+
+        // Sum the hours spent on each task type for the selected date
+        const taskTypes = {};
+        filteredData.forEach((task) => {
+          if (taskTypes.hasOwnProperty(task.taskType)) {
+            taskTypes[task.taskType] += task.hoursSpent;
+          } else {
+            taskTypes[task.taskType] = task.hoursSpent;
+          }
+        });
+        setTaskData(taskTypes);
+        console.log(taskTypes);
       } catch (err) {
         console.error(err);
       }
     };
-    handleClick();
-  }, []);
+
+    if (selectedDate) {
+      // If a selectedDate prop is provided, fetch data for the selected date
+      fetchTaskData(selectedDate);
+    } else {
+      // If no selectedDate prop is provided, fetch data for today's date
+      const today = new Date().toLocaleDateString().split("T")[0];
+      fetchTaskData(today);
+      console.log(today);
+    }
+
+    // Cleanup function to cancel the request if the component unmounts
+    return () => {
+      controller.abort();
+    };
+  }, [selectedDate, axiosPrivate, auth]);
+
+  console.log("Selected Date in HourSheet:", selectedDate);
+  console.log("Task Data:", taskData);
+
+  // Calculate the KPI dynamically based on taskData
+  const productionKPI = taskData.Production || 0;
+  const nonProductionKPI = taskData["Non-Production"] || 0;
+
+  // Calculate the percentage for the CircularProgressbar
+  const productionPercentage = (productionKPI / 7.5) * 100;
+  const nonProductionPercentage = (nonProductionKPI / 5.5) * 100;
+  const sum = productionPercentage + nonProductionPercentage;
+  const sumText = parseFloat(sum.toFixed(2));
+
+  console.log("Selected Date in HourSheet:", selectedDate);
+  console.log("Task Data:", taskData);
+  console.log("productionPercentage Data:", productionPercentage);
+  console.log("nonProductionPercentage Data:", nonProductionPercentage);
+
   return (
-    <div className="mt-2 shrink-0 rounded-3xl md:mb-[-3rem] md:mt-0">
-      <p className="ml-6 text-lg font-bold text-[#0D1829] md:text-center ">
-        Hour-Sheet
-      </p>
-      <div className="mx-5 mt-4 flex justify-center md:mt-5">
-        <div className="flex h-16 w-80 scale-100 items-center justify-around rounded-xl bg-[#2051E5] text-lg font-semibold text-white">
-          <p
-            className="leading-[3rem] 
-"
-          >
-            Production
-          </p>
-          {/* <p>{userInfo.hoursSpent}</p> */}
+    <div className="mb-10 mt-2 shrink-0 px-10 pt-5 md:flex md:justify-evenly ">
+      {/* EFFICIENCY */}
+      <div className=" ">
+        <h1 className="ml-6 text-lg font-bold text-[#0D1829] md:text-center">
+          Efficiency
+        </h1>
+
+        <div className="  flex justify-center  px-12 ">
+          <div className=" m-4 flex h-auto min-w-[374px] scale-100 justify-around gap-2 rounded-xl bg-[#EEF2FF] py-2   ">
+            <div className=" ">
+              <div className="ml-1 py-2  text-sm font-medium">
+                Candidate Efficiency
+              </div>
+              <p className="ml-40 pb-2 text-xs font-normal">in hrs</p>
+              {/* Map taskType and hours in efficiency */}
+              {Object.entries(taskData)
+                .sort(([taskTypeA], [taskTypeB]) => {
+                  if (
+                    taskTypeA === "Production" &&
+                    taskTypeB === "Non-Production"
+                  ) {
+                    return -1; // "Production" should come before "Non-Production"
+                  } else if (
+                    taskTypeA === "Non-Production" &&
+                    taskTypeB === "Production"
+                  ) {
+                    return 1; // "Non-Production" should come after "Production"
+                  } else {
+                    return 0; // Preserve the original order
+                  }
+                })
+                .map(([taskType, hours]) => (
+                  <div
+                    key={taskType}
+                    className="flex items-center justify-between gap-2  "
+                  >
+                    <span className="flex items-center gap-3">
+                      <Icon
+                        color={
+                          taskType === "Non-Production" ? "#F77307" : "#2051E5"
+                        }
+                      >
+                        <CheckboxIndeterminate16Regular />
+                      </Icon>
+                      <p className="opacity-60">{taskType}</p>
+                    </span>
+
+                    <div className=" flex flex-col items-center  ">
+                      <p className=" ml-[-30px] opacity-60">{hours}</p>
+                    </div>
+                  </div>
+                ))}
+              <p className="ml-4 pb-2 pt-6 text-sm font-medium text-[#0D1829]">
+                Overall: {`${sumText}%`}
+              </p>
+            </div>
+            <div className="mr-7 mt-6 ">
+              <div className="w-16">
+                <div className="ml-4 text-2xl">
+                  <CircularProgressbarWithChildren
+                    value={sum}
+                    text={`${sumText}%`}
+                    styles={buildStyles({
+                      pathColor: "#2051E5",
+                      trailColor: "#eee",
+                      strokeLinecap: "butt",
+                      textSize: "20px"
+                    })}
+                  >
+                    <CircularProgressbar
+                      value={nonProductionPercentage}
+                      styles={buildStyles({
+                        pathColor: "#F77307",
+                        trailColor: "transparent",
+                        strokeLinecap: "butt",
+                        textSize: "20px"
+                      })}
+                    />
+                  </CircularProgressbarWithChildren>
+                </div>
+                <div className="mr-3 mt-7 flex flex-col items-center">
+                  <p className="whitespace-nowrap text-xs font-normal opacity-60">
+                    Due Date
+                  </p>
+                  <div className="flex items-center gap-2 whitespace-nowrap font-medium">
+                    <Icon color="#333" size="30">
+                      <CalendarMonthOutlined />
+                    </Icon>
+                    <p className="text-xs font-medium opacity-90">
+                      June 6, 2022
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="m-5 flex justify-center md:mt-6">
-        <div className=" flex h-16 w-80 scale-100 items-center justify-around rounded-xl bg-[#F77307] text-lg font-semibold text-white    md:mb-12">
-          <p
-            className="leading-[3rem]  
-"
-          >
-            Non-production
-          </p>
-          <p>3 hrs</p>
-        </div>
+
+      <div className="mt-2 shrink-0 rounded-3xl md:mb-[-3rem] md:mt-0">
+        <p className="ml-6 text-lg font-bold text-[#0D1829] md:text-center">
+          Hour-Sheet
+        </p>
+        {Object.entries(taskData)
+          .sort(([taskTypeA], [taskTypeB]) => {
+            if (taskTypeA === "Production" && taskTypeB === "Non-Production") {
+              return -1; // "Production" should come before "Non-Production"
+            } else if (
+              taskTypeA === "Non-Production" &&
+              taskTypeB === "Production"
+            ) {
+              return 1; // "Non-Production" should come after "Production"
+            } else {
+              return 0; // Preserve the original order
+            }
+          })
+          .map(([taskType, hours]) => (
+            <div key={taskType} className="m-5 flex justify-center md:mt-6">
+              <div
+                className={`flex h-16 w-80 scale-100 items-center justify-between rounded-xl ${
+                  taskType === "Non-Production"
+                    ? "bg-[#F77307]"
+                    : "bg-[#2051E5]"
+                } px-8 text-lg font-semibold text-white`}
+              >
+                <p className="leading-[3rem]">{taskType}</p>
+                <p>{hours} hrs</p>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
