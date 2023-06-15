313b7f0e2393e971afe234ccc5fe8cb0378da4bb
Rahul Gupta <rahul123alok@gmail.com>
2023-06-15 18:25:10 +0530

    Update Rectangle.jsx

--- Client/src/components/Rectangle.jsx
+++ Client/src/components/Rectangle.jsx
@@ -1,7 +1,7 @@
 import PopUp from "./PopUp";
 const Rectangle = () => {
   return (
-    <div className="m-5 flex justify-center md:mt-6">
+    <div className="m-5 flex justify-center pb-10 ">
       <button className="flex h-16 w-80 scale-100 items-center justify-center  rounded-xl bg-gradient-to-r from-blue-800 to-indigo-900 px-8 text-3xl font-extrabold   text-white">
         <p className="leading-[3rem]">+</p>
       </button>
