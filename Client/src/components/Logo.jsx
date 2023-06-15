0d756757f06936c126ba5cf2bdaea4124d657180
Rahul Gupta <rahul123alok@gmail.com>
2023-06-15 18:24:07 +0530

    Modified Logo

--- Client/src/components/Logo.jsx
+++ Client/src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Logo } from "../../config";
 const Login = () => {
   return (
     <div className="bg-white">
-      <img className="mx-auto flex justify-center" src={Logo} />
+      <img className="mx-auto flex justify-center pl-2" src={Logo} />
       <hr className="border-1 border-gray-300 drop-shadow-xl hover:border-gray-500" />
     </div>
   );
